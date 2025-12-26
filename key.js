523d8504589505cfa9484bcdec6653ed75b7025b
I666tima <41543324+I666tima@users.noreply.github.com>
2025-12-26 12:32:53 +0700

    Update and rename node.js to key.js

---
+++ key.js
@@ -0,0 +1,32 @@
+// Подключаем dotenv и указываем путь к файлу .env
+require('dotenv').config({ path: /storage/emulated/0/Download/Меню Telegram/.env });
+
+const { Telegraf } = require('telegraf');
+
+// Берём токен и URL из переменных окружения
+const BOT_API_TOKEN = process.env.BOT_API_TOKEN;
+const WEBAPP_URL = https://github.com/I666tima/Grooming_pushistiki.github.io/blob/m;
+
+// Проверка: если токен не найден
+if (!BOT_API_TOKEN) {
+  throw new Error('Токен бота не найден в меню/.env!');
+}
+
+const bot = new Telegraf(BOT_API_TOKEN);
+
+bot.start((ctx) => {
+  return ctx.reply('Выбрать стрижку', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: 'Выбрать стрижку',
+            web_app: { url: WEBAPP_URL }
+          }
+        ]
+      ]
+    }
+  });
+});
+
+bot.launch();
